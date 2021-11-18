c0941d2124347267d7fa6d1cf13e0b02b46ca5e3
Lei Zhanyao <leizhanyao@gmail.com>
2021-11-18 14:22:55 +0800

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+const babel = require("@babel/core");
+const fs = require('fs')
+inputCodePath = 'inputCode/example.js'
+outputCodePath = 'outputCode/output.js'
+pluginPath = './myVisitor.js'
+
+const inputCode = fs.readFileSync(inputCodePath, 'utf8')
+console.log(inputCode)
+
+const { code } = babel.transform(inputCode, {
+    plugins: [
+        pluginPath
+    ]
+})
+
+console.log(code); // var b = 1
+fs.writeFileSync(outputCodePath, code, {flag: 'w+'});
---
+++ outputCode/output.js
@@ -0,0 +1,5 @@
+function a() {
+  console.log("[enter] function a");
+  return 1;
+  console.log("[exit ] function a");
+}
